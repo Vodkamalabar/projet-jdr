a414b7bb053ef3b6956fd82fa922a0f3f2deed8b
Julien Bulot <bulot_j@etna-alternance.net>
2013-11-01 19:41:52 +0100

    add quete1.h

---
+++ quete1/quete1.h
@@ -0,0 +1,28 @@
+/*
+** quete1.h for quete1 in /Users/julienbulot/Documents/projet-jdr/quete1
+** 
+** Made by BULOT Julien
+** Login   <bulot_j@etna-alternance.net>
+** 
+** Started on  Fri Nov  1 19:35:53 2013 BULOT Julien
+** Last update Fri Nov  1 19:39:44 2013 BULOT Julien
+*/
+
+#ifndef __QUETE1_H__
+# define __QUETE1_H__
+
+typedef struct	s_hero
+{
+  char		*name;
+  int		pv;
+  int		pm;
+}		t_hero;
+
+typedef struct	s_monster
+{
+  char		*name;
+  int		pv;
+  int		pm;
+}		t_monster;
+
+#endif

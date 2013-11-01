cdd73c8cefb6c2eb61f457823a7ff6a18c3d13a1
Julien Bulot <bulot_j@etna-alternance.net>
2013-11-01 20:01:22 +0100

    Makefile

---
+++ quete1/main.c
@@ -0,0 +1,19 @@
+/*
+** main.c for quete1 in /Users/julienbulot/Documents/projet-jdr/quete1
+** 
+** Made by BULOT Julien
+** Login   <bulot_j@etna-alternance.net>
+** 
+** Started on  Fri Nov  1 19:43:44 2013 BULOT Julien
+** Last update Fri Nov  1 19:56:31 2013 BULOT Julien
+*/
+
+#include <stdlib.h>
+#include "my.h"
+#include "quete1.h"
+
+int	main(int argc, char **argv)
+{
+  
+  return (EXIT_SUCCESS);
+}
